766925c53596d04eb529c4970f9f5f50c40d3cae
hungtrang20111501 <hungtrang20111501@gmail.com>
2024-11-11 14:04:29 +0700

    feat: apply template (part 2)

---
+++ assets/js/config.js
@@ -0,0 +1,107 @@
+let biicore = {
+  templatePremium: true,
+  isPremium: true,
+  bgMusic: 'assets/audio/BeautifulInWhite-ShaneFilan-524801.mp3',
+  alert: '[]',
+  effect: { type: 'heart' },
+  isAutoPlay: false,
+};
+
+var photoGalleries = [
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae0f3395f427129073767/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae0f3395f427129073767/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae0cb7604de197800c927/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae0cb7604de197800c927/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae0aac528ddd5c4030deb/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae0aac528ddd5c4030deb/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae092c133c5a8550e5db0/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae092c133c5a8550e5db0/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae07d7604de197800c926/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae07d7604de197800c926/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae05245356fcf1309b3a1/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae05245356fcf1309b3a1/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae0345d14fd214a0928cf/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae0345d14fd214a0928cf/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae0123a892203590b808e/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae0123a892203590b808e/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adfe9c3eaef009508bc22/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adfe9c3eaef009508bc22/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae06c8af6d409de0ba89e/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672ae06c8af6d409de0ba89e/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672acf9b42b0c72b2f022ea0/galleries/672adfc5f53b17071e0673fc/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adfc5f53b17071e0673fc/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adf9e48a20777a80b4a60/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adf9e48a20777a80b4a60/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adf819346b3840200618e/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adf819346b3840200618e/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adf6897e804ebdb01fc80/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adf6897e804ebdb01fc80/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adf48fafc419dec006e1b/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adf48fafc419dec006e1b/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+  {
+    src: 'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adf2d85f55915da071a52/large.jpg',
+    thumb:
+      'https://cdn.biihappy.com/ziiweb/website/672acf9b42b0c72b2f022ea0/galleries/672adf2d85f55915da071a52/small.jpg',
+    subHtml: `<div class="lg-sub-html"><h4></h4></div>`,
+  },
+];

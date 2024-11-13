07ce1773b143e8d469d680b1404f12649c0746fd
hungtrang20111501 <hungtrang20111501@gmail.com>
2024-11-13 10:11:49 +0700

    feat: remove shake animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -683,29 +683,6 @@ flyingPages();
         '@media (max-width: 799px){#menu-access{bottom: 238px!important;}}';
     }
   });
-  function shakeTooltip() {
-    var arrTooltip = $('ul.list-menu-icon').children();
-    arrTooltip.each(function (index) {
-      setTimeout(() => {
-        if (document.querySelector('.btn-menu-close').style.display !== 'none') {
-          $(this).addClass('shake');
-          $(this).children().children().children('.tooltiptext').css('visibility', 'visible');
-          setTimeout(() => {
-            $(this).children().children().children('.tooltiptext').css('visibility', '');
-            $(this).removeClass('shake');
-          }, 3000);
-        } else {
-          return false;
-        }
-      }, index * 5000);
-    });
-  }
-  if ($('#menu-access').length > 0) {
-    setTimeout(() => {
-      shakeTooltip();
-      myInterval = setInterval(shakeTooltip, 20000);
-    }, 3000);
-  }
   $('.btn-menu-close').click(function () {
     $('tooltiptext').css('visibility', '');
     clearInterval(myInterval);

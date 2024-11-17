1d4b8fdea96fc1c167fceefc4e41a532f2b128b6
hungtrang20111501 <hungtrang20111501@gmail.com>
2024-11-17 09:56:32 +0700

    refactor: optimize code

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,11 +128,14 @@ flyingPages();
 
 (function ($) {
   'use strict';
+
   window.FPConfig = { delay: 0, ignoreKeywords: [], maxRPS: 3, hoverDelay: 50 };
+
   $(window).on('load', function () {
     $('.spinner').fadeOut();
     $('.preloader-area').delay(350).fadeOut('slow');
   });
+
   jQuery(document).ready(function ($) {
     $(window).on('scroll', function () {
       if ($(window).scrollTop() > 50) {
@@ -141,132 +144,10 @@ flyingPages();
         $('.navbar-fixed-top').removeClass('sticky');
       }
     });
+
     var wow = new WOW();
     wow.init();
-    $('.slider-active').owlCarousel({
-      items: 1,
-      dots: true,
-      autoplay: true,
-      loop: true,
-      mouseDrag: false,
-      touchDrag: false,
-      navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-      responsive: { 0: { items: 1 }, 600: { items: 1 }, 1000: { items: 1, nav: true } },
-    });
-    $('.wishes-owl').owlCarousel({
-      items: 1,
-      loop: true,
-      margin: 10,
-      navigation: false,
-      responsiveClass: true,
-      responsive: {
-        0: { items: 1, nav: false },
-        600: { items: 1, nav: false },
-        1000: { items: 1, nav: true, loop: false },
-      },
-    });
-    $('.wishes-owl1').owlCarousel({
-      items: 2,
-      loop: true,
-      margin: 10,
-      navigation: false,
-      responsiveClass: true,
-      responsive: {
-        0: { items: 1, nav: false },
-        600: { items: 1, nav: false },
-        1000: { items: 2, nav: true, loop: false },
-      },
-    });
-    $('.wishes-owl2').owlCarousel({
-      items: 2,
-      loop: true,
-      margin: 10,
-      navigation: false,
-      responsiveClass: true,
-      responsive: {
-        0: { items: 1, nav: false },
-        600: { items: 1, nav: false },
-        1000: { items: 2, nav: true, loop: false },
-      },
-    });
-    $('#testimonial-slider').owlCarousel({
-      items: 1,
-      pagination: true,
-      navigationText: ['', ''],
-      slideSpeed: 1000,
-      autoPlay: true,
-      responsive: { 0: { items: 1 }, 600: { items: 1 }, 1000: { items: 1 } },
-    });
-    $('.owl-registry').owlCarousel({
-      loop: true,
-      margin: 10,
-      dots: false,
-      navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-      autoplay: true,
-      slideSpeed: 2000,
-      responsiveClass: true,
-      responsive: {
-        0: { items: 1, nav: true },
-        500: { items: 2, nav: true },
-        600: { items: 3, nav: true },
-        1000: { items: 4, nav: true, loop: false },
-      },
-    });
-    $('.store-slider').slick({
-      infinite: true,
-      autoplay: false,
-      arrows: true,
-      dots: false,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      responsive: [
-        { breakpoint: 991, settings: { slidesToShow: 3, slidesToScroll: 1, infinite: true } },
-        { breakpoint: 767, settings: { slidesToShow: 2, slidesToScroll: 1, infinite: true } },
-        { breakpoint: 479, settings: { slidesToShow: 1, slidesToScroll: 1, infinite: true } },
-      ],
-    });
-    $('.services-slider').slick({
-      infinite: true,
-      autoplay: true,
-      arrows: true,
-      dots: false,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [
-        { breakpoint: 991, settings: { slidesToShow: 2, slidesToScroll: 1, infinite: true } },
-        { breakpoint: 479, settings: { slidesToShow: 1, slidesToScroll: 1, infinite: true } },
-      ],
-    });
-    $('.store-slider1').slick({
-      infinite: true,
-      autoplay: false,
-      arrows: true,
-      dots: false,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: [
-        { breakpoint: 991, settings: { slidesToShow: 3, slidesToScroll: 1, infinite: true } },
-        { breakpoint: 767, settings: { slidesToShow: 2, slidesToScroll: 1, infinite: true } },
-        { breakpoint: 479, settings: { slidesToShow: 1, slidesToScroll: 1, infinite: true } },
-      ],
-    });
-    $('.slider-for').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      arrows: false,
-      dots: false,
-      fade: true,
-      asNavFor: '.slider-nav',
-    });
-    $('.slider-nav').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      asNavFor: '.slider-for',
-      dots: false,
-      centerMode: true,
-      focusOnSelect: true,
-    });
+
     $('.navbar-nav').on('click', 'a', function (e) {
       try {
         var anchor = $(this);
@@ -276,68 +157,15 @@ flyingPages();
         e.preventDefault();
       } catch (error) {}
     });
+
     $('.navbar-nav').on('click', 'a', function () {
       $('.navbar-collapse').removeClass('in');
     });
-    $('.skillsection').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
-      if (visible) {
-        $('.chart').easyPieChart({
-          easing: 'easeOut',
-          delay: 3000,
-          lineCap: 'square',
-          lineWidth: '10',
-          barColor: '#fff',
-          trackColor: 'transparent',
-          scaleColor: false,
-          lineWidth: 2,
-          size: 110,
-          animate: 2000,
-          onStep: function (from, to, percent) {
-            this.el.children[0].innerHTML = Math.round(percent);
-          },
-        });
-        $(this).unbind('inview');
-      }
-    });
+
     $('.progress .progress-bar').css('width', function () {
       return $(this).attr('aria-valuenow') + '%';
     });
-    $('.portfolio-inner').mixItUp();
-    $('.coundown_res').countdown('2018/01/02', function (event) {
-      var $this = $(this);
-      $this.find('#day').html(event.strftime('<span>%D</span>'));
-      $this.find('#hour').html(event.strftime('<span>%H</span>'));
-      $this.find('#month').html(event.strftime('<span>%M</span>'));
-      $this.find('#second').html(event.strftime('<span>%S</span>'));
-    });
-    $('.counter').counterUp();
-    try {
-      $('.fun-facts_wrapper').appear(function () {
-        $('.timer').countTo();
-      });
-    } catch (err) {
-      console.log(err.message);
-    }
-    $('.video-play-btn').magnificPopup({ type: 'iframe' });
-    $('.project-hover').magnificPopup({ delegate: 'a', type: 'image' });
-    var magnifPopup = function () {
-      $('.port-popup').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        removalDelay: 300,
-        mainClass: 'mfp-with-zoom',
-        gallery: { enabled: true },
-        zoom: {
-          enabled: true,
-          duration: 300,
-          easing: 'ease-in-out',
-          opener: function (openerElement) {
-            return openerElement.is('img') ? openerElement : openerElement.find('img');
-          },
-        },
-      });
-    };
-    magnifPopup();
+
     function masonryGridSetting() {
       if ($('.masonry-gallery').length) {
         var $grid = $('.masonry-gallery').masonry({
@@ -356,9 +184,11 @@ flyingPages();
 
     // SLIDER
     var menu = [];
+
     jQuery('.swiper-slide').each(function (index) {
       menu.push(jQuery(this).find('.slide-inner').attr('data-text'));
     });
+
     var interleaveOffset = 0.5;
     var swiperOptions = {
       loop: true,
@@ -367,12 +197,10 @@ flyingPages();
         delay: 5000,
         disableOnInteraction: true,
       },
-
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
       },
-
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -383,6 +211,7 @@ flyingPages();
 
     // DATA BACKGROUND IMAGE
     var sliderBgSetting = $('.slide-bg-image');
+
     sliderBgSetting.each(function (indx) {
       if ($(this).attr('data-background')) {
         $(this).css('background-image', 'url(' + $(this).data('background') + ')');
@@ -393,99 +222,6 @@ flyingPages();
       setAutoHeightEventImage();
       $('[data-toggle="popover"]').popover();
     });
-
-    if ($('#wish-form').length) {
-      $('#wish-form').validate({
-        rules: {
-          name: {
-            required: true,
-            minlength: 5,
-          },
-          content: {
-            required: true,
-            minlength: 10,
-          },
-          email: {
-            required: false,
-            email: true,
-          },
-        },
-
-        messages: {
-          name: {
-            required: 'Vui lÃ²ng nháº­p tÃªn cá»§a báº¡n.',
-            minlength: 'TÃªn pháº£i lá»›n hÆ¡n 5 kÃ½ tá»±.',
-          },
-          content: {
-            required: 'Vui lÃ²ng nháº­p lá»i chÃºc.',
-            minlength: 'Lá»i chÃºc pháº£i lá»›n hÆ¡n 10 kÃ½ tá»±.',
-          },
-          email: {
-            email: 'Äá»‹a chá»‰ email khÃ´ng há»£p lá»‡.',
-          },
-        },
-
-        errorPlacement: function (error, element) {
-          if (element.attr('name') == 'content') {
-            error.insertAfter('#wish-form .vitualTextarea');
-          } else {
-            error.insertAfter(element);
-          }
-        },
-        submitHandler: function (form) {
-          $('#loader').css('display', 'inline-block');
-          $.ajax({
-            type: 'POST',
-            url: '/wish',
-            data: $(form).serialize(),
-            success: function (res) {
-              $('#loader').hide();
-              if (!res.error) {
-                $('.wish-box').scrollTop(0);
-                $('.wish-box').prepend(
-                  '<div class="wish-box-item bg"><strong>' +
-                    $(form)
-                      .find("input[name='name']")
-                      .val()
-                      .replace(/&/g, '&amp;')
-                      .replace(/>/g, '&gt;')
-                      .replace(/</g, '&lt;')
-                      .replace(/"/g, '&quot;') +
-                    '</strong><p>' +
-                    $(form)
-                      .find("textarea[name='content']")
-                      .val()
-                      .replace(/&/g, '&amp;')
-                      .replace(/>/g, '&gt;')
-                      .replace(/</g, '&lt;')
-                      .replace(/"/g, '&quot;') +
-                    '</p></div>',
-                );
-                $('#success').html(res.message).slideDown('slow');
-                setTimeout(function () {
-                  $('#success').slideUp('slow');
-                }, 5000);
-              } else {
-                $('#error').html(res.message).slideDown('slow');
-                setTimeout(function () {
-                  $('#error').slideUp('slow');
-                }, 5000);
-              }
-
-              form.reset();
-            },
-            error: function () {
-              $('#loader').hide();
-              $('#error').slideDown('slow');
-              setTimeout(function () {
-                $('#error').slideUp('slow');
-              }, 5000);
-            },
-          });
-          return false;
-        },
-      });
-    }
 
     if ($('#donate-modal').length && $('.buttonDonate').length && $('.donate-modal-close').length) {
       $(document).on('click', '.buttonDonate', function () {
@@ -645,6 +381,7 @@ flyingPages();
 
     sticky();
   });
+
   function setAutoHeightEventImage() {
     if ($('.wedding-event').length) {
       if ($(window).width() >= 992) {
@@ -667,17 +404,20 @@ flyingPages();
     $('.btn-menu-close').show();
     $('.btn-menu-open').hide();
   });
+
   $('.btn-menu-close').click(function () {
     $('ul.list-menu-icon').css('opacity', '0');
     $('ul.list-menu-icon').css('pointer-events', 'none');
     $('.btn-menu-open').show();
     $('.btn-menu-close').hide();
   });
+
   setTimeout(() => {
     $('.btn-menu-open').hide();
     $('.btn-menu-close').show();
     $('ul.list-menu-icon').css('opacity', '1');
   }, 3000);
+
   $(window).on('load', function () {
     if ($('.bii-logo').length > 0) {
       $('#menu-access').css('bottom', '278px');
@@ -685,6 +425,7 @@ flyingPages();
         '@media (max-width: 799px){#menu-access{bottom: 238px!important;}}';
     }
   });
+
   $(document).on('click', '#donate-modal .crypto-item', function () {
     let parent = $(this).parents('.donate-card');
     parent.find('.cryptos-box-view').show();

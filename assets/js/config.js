ba3a02bf0d86ae38f260bde1d7d7311e3e6d8508
hungtrang20111501 <hungtrang20111501@gmail.com>
2024-12-19 22:42:53 +0700

    feat: update gallery

--- assets/js/config.js
+++ assets/js/config.js
@@ -16,9 +16,9 @@ var photoGalleries = [
     subHtml: `<div class="lg-sub-html"><h4>Tình yêu là điều kiện trong đó hạnh phúc của người khác là điều cần thiết cho chính bạn</h4></div>`,
   },
   {
-    src: 'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG04424.webp',
+    src: 'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG05412.webp',
     thumb:
-      'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG04424.webp',
+      'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG05412.webp',
     subHtml: `<div class="lg-sub-html"><h4>Tình yêu không chỉ là một danh từ - nó là một động từ; nó còn hơn cả một cảm giác - đó là sự quan tâm, chia sẻ, giúp đỡ, hy sinh</h4></div>`,
   },
   {
@@ -28,9 +28,9 @@ var photoGalleries = [
     subHtml: `<div class="lg-sub-html"><h4>Vì vậy, để được ai đó nhìn thấy đầy đủ và được yêu mến dù thế nào đi nữa — đây là một sự dâng hiến của con người có thể là điều kỳ diệu</h4></div>`,
   },
   {
-    src: 'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG04905.webp',
+    src: 'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG04722.webp',
     thumb:
-      'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG04905.webp',
+      'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG04722.webp',
     subHtml: `<div class="lg-sub-html"><h4>Tình yêu không làm cho thế giới quay tròn. Tình yêu là những gì làm cho chuyến đi đáng giá</h4></div>`,
   },
   {
@@ -40,28 +40,16 @@ var photoGalleries = [
     subHtml: `<div class="lg-sub-html"><h4>Tình yêu không có rào cản. Nó nhảy rào, nhảy rào, xuyên tường để đến đích với đầy hy vọng</h4></div>`,
   },
   {
-    src: 'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG04722.webp',
-    thumb:
-      'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG04722.webp',
-    subHtml: `<div class="lg-sub-html"><h4>Tôi yêu bạn vì tất cả những gì bạn đang có, tất cả những gì bạn đã có, và tất cả những gì bạn chưa hiện hữu</h4></div>`,
-  },
-  {
     src: 'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG05187.webp',
     thumb:
       'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG05187.webp',
-    subHtml: `<div class="lg-sub-html"><h4>Tôi có thể chinh phục thế giới bằng một tay miễn là bạn đang nắm tay kia</h4></div>`,
-  },
-  {
-    src: 'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG05412.webp',
-    thumb:
-      'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG05412.webp',
-    subHtml: `<div class="lg-sub-html"><h4>Tình yêu đích thực đứng về phía nhau trong những ngày tốt đẹp và sát cánh hơn trong những ngày tồi tệ</h4></div>`,
+    subHtml: `<div class="lg-sub-html"><h4>Tôi yêu bạn vì tất cả những gì bạn đang có, tất cả những gì bạn đã có, và tất cả những gì bạn chưa hiện hữu</h4></div>`,
   },
   {
     src: 'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG05567.webp',
     thumb:
       'https://cdn.jsdelivr.net/gh/hungtrang20111501/hungtrang20111501.github.io/assets/images/wedding/HUG05567.webp',
-    subHtml: `<div class="lg-sub-html"><h4>Được ai đó yêu sâu sắc sẽ mang lại cho bạn sức mạnh, trong khi yêu ai đó sâu sắc sẽ cho bạn dũng khí</h4></div>`,
+    subHtml: `<div class="lg-sub-html"><h4>Tôi có thể chinh phục thế giới bằng một tay miễn là bạn đang nắm tay kia</h4></div>`,
   },
 ];
 
